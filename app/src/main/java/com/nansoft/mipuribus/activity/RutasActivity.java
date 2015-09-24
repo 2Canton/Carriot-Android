4d72d4f68b7a5aea3256eb197af14e210348bc11
Carlos Castro Brenes <ccasbre27@hotmail.com>
2015-09-24 09:48:12 -0600

    Se agrega la verificación de versiones

--- app/src/main/java/com/nansoft/mipuribus/activity/RutasActivity.java
+++ app/src/main/java/com/nansoft/mipuribus/activity/RutasActivity.java
@@ -2,6 +2,9 @@ package com.nansoft.mipuribus.activity;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 import com.microsoft.windowsazure.mobileservices.MobileServiceList;
 import com.microsoft.windowsazure.mobileservices.table.query.QueryOrder;
 import com.nansoft.mipuribus.R;
@@ -10,11 +13,13 @@ import com.nansoft.mipuribus.helper.Util;
 import com.nansoft.mipuribus.adapter.RutaAdapterListView;
 import com.nansoft.mipuribus.model.CarreraRuta;
 import com.nansoft.mipuribus.model.Horario;
+import com.nansoft.mipuribus.model.Version;
 import com.nansoft.mipuribus.model.parada;
 import com.nansoft.mipuribus.model.Ruta;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -43,6 +48,13 @@ public class RutasActivity extends Activity
 	View includedLayout;
 
 	HelperDatabase objHandlerDataBase;
+
+	private static final int VERSION_BASEDATOS = 1;
+	private SharedPreferences prefs;
+	private String nombrePref = "Preferencias";
+
+	Version OBJ_VERSION;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
@@ -103,7 +115,10 @@ public class RutasActivity extends Activity
 		if(!objUtil.inicializarBaseDatos())
 			Toast.makeText(getApplicationContext(),"Ha ocurrido un error al inicializar la copia local",Toast.LENGTH_SHORT).show();
 
+		SharedPreferences prefs = getSharedPreferences(nombrePref, MODE_PRIVATE);
 
+		int VERSION_BASEDATOS_ACTUAL = prefs.getInt("VERSION_BASEDATOS",1);
+		OBJ_VERSION = new Version(VERSION_BASEDATOS_ACTUAL);
 
 		sincronizarDatos();
 	}
@@ -154,157 +169,194 @@ public class RutasActivity extends Activity
 
 	private void sincronizarDatos()
 	{
-		if (Util.isNetworkAvailable(getApplicationContext())) {
+		// verificamos la versión de base de datos
+		final ListenableFuture <Version> resultVersion = Util.mClient.invokeApi("version","GET",null,Version.class);
 
-			includedLayout.setVisibility(View.GONE);
-			mSwipeRefreshLayout.setEnabled(false);
+		// agregamos un callback
+		Futures.addCallback(resultVersion, new FutureCallback<Version>() {
+			@Override
+			public void onFailure(Throwable exc) {
+			}
 
-			new AsyncTask<Void, Void, Boolean>() {
+			@Override
+			public void onSuccess(Version result) {
 
-				MobileServiceSyncTable<Ruta> rutaTable;
-				MobileServiceSyncTable<Horario> horarioTable;
-				MobileServiceSyncTable<CarreraRuta> carreraRutaTable;
-				MobileServiceSyncTable<parada> paradaTable;
+				// establecemos la versión remota en el objeto
+				OBJ_VERSION.setVersionRemota(result.getVersionRemota());
 
-				Query mPullQueryRuta;
-				Query mPullQueryHorario;
-				Query mPullQueryCarreraRuta;
-				Query mPullQueryParada;
+				// guardamos en las preferencias de usuario la versión de base de datos que tenemos
+				savePreferences(OBJ_VERSION.getVersionRemota());
 
-
-
-
-				@Override
-				protected void onPreExecute()
+				// verificamos si se debe actualizar
+				if (OBJ_VERSION.estadoActualizarBaseDatos())
 				{
-					// referencia a las tablas que se va usar
-					rutaTable = Util.mClient.getSyncTable("Ruta", Ruta.class);
-					horarioTable = Util.mClient.getSyncTable("Horario", Horario.class);
-					carreraRutaTable = Util.mClient.getSyncTable("CarreraRuta", CarreraRuta.class);
-					paradaTable = Util.mClient.getSyncTable("Parada", parada.class);
 
-					mPullQueryRuta = Util.mClient.getTable(Ruta.class).orderBy("nombre", QueryOrder.Ascending);
-					mPullQueryHorario = Util.mClient.getTable("Horario",Horario.class).top(10);
-					mPullQueryCarreraRuta = Util.mClient.getTable("CarreraRuta",CarreraRuta.class).top(1000);
-					mPullQueryParada = Util.mClient.getTable("Parada",parada.class).top(100);
+					// si es así verificamos si hay internet
+					if (Util.isNetworkAvailable(getApplicationContext()))
+					{
 
-					// se limpia el adapter mientras carga
-					mAdapter.clear();
+						includedLayout.setVisibility(View.GONE);
+						mSwipeRefreshLayout.setEnabled(false);
 
+						new AsyncTask<Void, Void, Boolean>() {
 
+							MobileServiceSyncTable<Ruta> rutaTable;
+							MobileServiceSyncTable<Horario> horarioTable;
+							MobileServiceSyncTable<CarreraRuta> carreraRutaTable;
+							MobileServiceSyncTable<parada> paradaTable;
 
-					// se elimina base de datos
-					HelperDatabase.db.delete("Ruta", null, null);
-					HelperDatabase.db.delete("Horario", null, null);
-					HelperDatabase.db.delete("CarreraRuta", null, null);
-					HelperDatabase.db.delete("Parada", null, null);
+							Query mPullQueryRuta;
+							Query mPullQueryHorario;
+							Query mPullQueryCarreraRuta;
+							Query mPullQueryParada;
 
 
-				}
+							@Override
+							protected void onPreExecute() {
+								// referencia a las tablas que se va usar
+								rutaTable = Util.mClient.getSyncTable("Ruta", Ruta.class);
+								horarioTable = Util.mClient.getSyncTable("Horario", Horario.class);
+								carreraRutaTable = Util.mClient.getSyncTable("CarreraRuta", CarreraRuta.class);
+								paradaTable = Util.mClient.getSyncTable("Parada", parada.class);
 
-				@Override
-				protected Boolean doInBackground(Void... params) {
-					try {
-						//se cargan los últimos cambios
-						rutaTable.pull(mPullQueryRuta).get();
-						horarioTable.pull(mPullQueryHorario).get();
-						carreraRutaTable.pull(mPullQueryCarreraRuta).get();
-						paradaTable.pull(mPullQueryParada).get();
+								mPullQueryRuta = Util.mClient.getTable(Ruta.class).orderBy("nombre", QueryOrder.Ascending);
+								mPullQueryHorario = Util.mClient.getTable("Horario", Horario.class).top(10);
+								mPullQueryCarreraRuta = Util.mClient.getTable("CarreraRuta", CarreraRuta.class).top(1000);
+								mPullQueryParada = Util.mClient.getTable("Parada", parada.class).top(100);
 
-						final MobileServiceList<Ruta> resultRuta = rutaTable.read(mPullQueryRuta).get();
-						final MobileServiceList <Horario> resultHorario = horarioTable.read(mPullQueryHorario).get();
-						final MobileServiceList <CarreraRuta> resultCarreraRuta = carreraRutaTable.read(mPullQueryCarreraRuta).get();
-						final MobileServiceList <parada> resultParada = paradaTable.read(mPullQueryParada).get();
+								// se limpia el adapter mientras carga
+								mAdapter.clear();
 
 
+								// se elimina base de datos
+								HelperDatabase.db.delete("Ruta", null, null);
+								HelperDatabase.db.delete("Horario", null, null);
+								HelperDatabase.db.delete("CarreraRuta", null, null);
+								HelperDatabase.db.delete("Parada", null, null);
 
-							runOnUiThread(new Runnable() {
 
-								@Override
-								public void run() {
+							}
 
-									// se agregan los registros en la base de datos
-									for (Ruta ruta: resultRuta)
-									{
-										objHandlerDataBase.InsertarRuta(ruta);
-										mAdapter.add(ruta);
-										mAdapter.notifyDataSetChanged();
+							@Override
+							protected Boolean doInBackground(Void... params) {
+								try {
+
+									//se cargan los últimos cambios
+									rutaTable.pull(mPullQueryRuta).get();
+									horarioTable.pull(mPullQueryHorario).get();
+									carreraRutaTable.pull(mPullQueryCarreraRuta).get();
+									paradaTable.pull(mPullQueryParada).get();
+
+									final MobileServiceList<Ruta> resultRuta = rutaTable.read(mPullQueryRuta).get();
+									final MobileServiceList<Horario> resultHorario = horarioTable.read(mPullQueryHorario).get();
+									final MobileServiceList<CarreraRuta> resultCarreraRuta = carreraRutaTable.read(mPullQueryCarreraRuta).get();
+									final MobileServiceList<parada> resultParada = paradaTable.read(mPullQueryParada).get();
+
+
+									runOnUiThread(new Runnable() {
+
+										@Override
+										public void run() {
+
+											// se agregan los registros en la base de datos
+											for (Ruta ruta : resultRuta) {
+												objHandlerDataBase.InsertarRuta(ruta);
+												mAdapter.add(ruta);
+												mAdapter.notifyDataSetChanged();
+											}
+										}
+
+									});
+
+
+
+									for (Horario horario : resultHorario) {
+										objHandlerDataBase.InsertarHorario(horario);
 									}
+
+									for (CarreraRuta carreraRuta : resultCarreraRuta) {
+										objHandlerDataBase.InsertarCarrera(carreraRuta);
+									}
+
+									for (parada parada : resultParada) {
+										objHandlerDataBase.InsertarSitioSalida(parada);
+									}
+
+									// se cargan los registros de la base de datos local en el adapter
+									//objHandlerDataBase.CargarAdapter();
+
+
+									return true;
+								} catch (final Exception exception) {
+
+									return false;
 								}
 
-							});
+							}
+
+							@Override
+							protected void onPostExecute(Boolean success) {
+
+								mSwipeRefreshLayout.setRefreshing(false);
 
 
-						for (Horario horario: resultHorario)
-						{
-							objHandlerDataBase.InsertarHorario(horario);
+								mSwipeRefreshLayout.setEnabled(true);
+
+								if (!success) {
+									includedLayout.setVisibility(View.VISIBLE);
+								} else {
+
+									includedLayout.setVisibility(View.GONE);
+								}
+							}
+
+							@Override
+							protected void onCancelled() {
+								super.onCancelled();
+							}
+						}.execute();
+					} else {
+
+						if (objHandlerDataBase.VerificarDatosRuta()) {
+							objHandlerDataBase.CargarAdapter();
+						} else {
+
+							setContentView(R.layout.error);
+
+
 						}
-
-						for (CarreraRuta carreraRuta: resultCarreraRuta)
-						{
-							objHandlerDataBase.InsertarCarrera(carreraRuta);
-						}
-
-						for (parada parada: resultParada)
-						{
-							objHandlerDataBase.InsertarSitioSalida(parada);
-						}
-
-						// se cargan los registros de la base de datos local en el adapter
-						//objHandlerDataBase.CargarAdapter();
+						mSwipeRefreshLayout.setRefreshing(false);
 
 
-						return true;
-					} catch (final Exception exception) {
-
-						return false;
+						mSwipeRefreshLayout.setEnabled(true);
 					}
+				} else {
+					if (objHandlerDataBase.VerificarDatosRuta()) {
+						objHandlerDataBase.CargarAdapter();
+					} else {
 
-				}
+						setContentView(R.layout.error);
 
-				@Override
-				protected void onPostExecute(Boolean success) {
 
+					}
 					mSwipeRefreshLayout.setRefreshing(false);
 
 
 					mSwipeRefreshLayout.setEnabled(true);
-
-					if (!success) {
-						includedLayout.setVisibility(View.VISIBLE);
-					} else {
-						includedLayout.setVisibility(View.GONE);
-					}
 				}
 
-				@Override
-				protected void onCancelled() {
-					super.onCancelled();
-				}
-			}.execute();
-		}
-		else
-		{
-
-			if(objHandlerDataBase.VerificarDatosRuta())
-			{
-				objHandlerDataBase.CargarAdapter();
 			}
-			else
-			{
-
-				setContentView(R.layout.error);
+		});
 
 
-
-			}
-			mSwipeRefreshLayout.setRefreshing(false);
-
-
-			mSwipeRefreshLayout.setEnabled(true);
-		}
 	}
 
-
+	private void savePreferences(int pVersion)
+	{
+		prefs = getSharedPreferences(nombrePref, MODE_PRIVATE);
+		SharedPreferences.Editor editor = prefs.edit();
+		editor.putInt("VERSION_BASEDATOS", pVersion);
+		editor.commit();
+	}
 
 }
