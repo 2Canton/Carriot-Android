d9df0e4306ad0a6e595e6550a896978a19ccc532
Carlos Castro Brenes <ccasbre27@hotmail.com>
2015-09-24 08:47:14 -0600

    Se agrega clase version para saber la version de base de datos

---
+++ app/src/main/java/com/nansoft/mipuribus/model/Version.java
@@ -0,0 +1,12 @@
+package com.nansoft.mipuribus.model;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Carlos on 24/09/2015.
+ */
+public class Version
+{
+    @SerializedName("id")
+    public int Id;
+}
