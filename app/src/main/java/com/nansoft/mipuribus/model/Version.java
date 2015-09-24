41ddf5e3caa35bc76b42300b77131cd1b6fe6a0e
Carlos Castro Brenes <ccasbre27@hotmail.com>
2015-09-24 09:29:46 -0600

    se agregan métodos get y set, además del que indica cuando se debe actualizar la base de datos

--- app/src/main/java/com/nansoft/mipuribus/model/Version.java
+++ app/src/main/java/com/nansoft/mipuribus/model/Version.java
@@ -7,6 +7,43 @@ import com.google.gson.annotations.SerializedName;
  */
 public class Version
 {
-    @SerializedName("id")
-    public int Id;
+    @SerializedName("version_remota")
+    private int versionRemota;
+
+    private int versionLocal;
+
+    public Version(int pVersionLocal)
+    {
+        versionLocal = pVersionLocal;
+    }
+
+    public int getVersionRemota() {
+        return versionRemota;
+    }
+
+    public void setVersionRemota(int versionRemota) {
+        this.versionRemota = versionRemota;
+    }
+
+    public int getVersionLocal() {
+        return versionLocal;
+    }
+
+    public void setVersionLocal(int versionLocal) {
+        this.versionLocal = versionLocal;
+    }
+
+    public boolean estadoActualizarBaseDatos()
+    {
+        if(versionRemota != versionLocal)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
 }
