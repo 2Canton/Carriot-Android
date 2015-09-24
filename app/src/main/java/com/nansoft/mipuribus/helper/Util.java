fd4e4e474cf87e864cb0479423ae40d72fb28fc3
Carlos Castro Brenes <ccasbre27@hotmail.com>
2015-09-24 08:16:34 -0600

    Se eliminan algunos import innecesarios y se establece la inicialización de base de datos local

--- app/src/main/java/com/nansoft/mipuribus/helper/Util.java
+++ app/src/main/java/com/nansoft/mipuribus/helper/Util.java
@@ -3,10 +3,16 @@ package com.nansoft.mipuribus.helper;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.view.View;
+import android.widget.Toast;
 
 import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
 import com.microsoft.windowsazure.mobileservices.MobileServiceList;
+import com.microsoft.windowsazure.mobileservices.table.query.Query;
+import com.microsoft.windowsazure.mobileservices.table.query.QueryOrder;
 import com.microsoft.windowsazure.mobileservices.table.sync.MobileServiceSyncContext;
+import com.microsoft.windowsazure.mobileservices.table.sync.MobileServiceSyncTable;
 import com.microsoft.windowsazure.mobileservices.table.sync.localstore.ColumnDataType;
 import com.microsoft.windowsazure.mobileservices.table.sync.localstore.SQLiteLocalStore;
 import com.microsoft.windowsazure.mobileservices.table.sync.synchandler.SimpleSyncHandler;
@@ -20,14 +26,16 @@ import java.util.Map;
  * Created by Carlos on 21/09/2015.
  */
 public class Util {
-    private final String UrlMobileServices = "https://puriscal.azure-mobile.net/";
+    public final String UrlMobileServices = "https://puriscal.azure-mobile.net/";
     public static String UrlHorarioRuta = "https://puriscal.azure-mobile.net/api/horarioruta";
-    private final String LlaveMobileServices = "CtavDeXtaLeUclXFhrPrjLJiUeeEek84";
+    public final String LlaveMobileServices = "CtavDeXtaLeUclXFhrPrjLJiUeeEek84";
 
     public static MobileServiceClient mClient;
 
     private Context mContext;
 
+    private MobileServiceSyncTable<Ruta> rutaTable;
+    private Query mPullQuery;
 
     public static boolean isNetworkAvailable(Context context) {
         ConnectivityManager connectivityManager
@@ -109,4 +117,6 @@ public class Util {
         }
 
     }
+
+
 }
